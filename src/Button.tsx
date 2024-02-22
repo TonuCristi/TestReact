aa55c96bc555e86c44e086de7fd9b560436e413c
Tonu Cristian <chrisdev2002@gmail.com>
2024-02-22 16:52:52 +0200

    update button text

--- src/Button.tsx
+++ src/Button.tsx
@@ -1,6 +1,6 @@
 export default function Button() {
   return (
-    <button className="button" onClick={() => console.log("It works!")}>
+    <button className="button" onClick={() => console.log("Very good!")}>
       Click me
     </button>
   );
